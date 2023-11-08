e1bc8fa798984fb425334aa80b9ffc5a97c7386d
A <anthonyc.animba@gmail.com>
2023-11-09 01:20:38 +0700

    .

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -13,20 +13,26 @@ const Hero: React.FC = () => {
 
   return (
     <div
-      className="hero w-full max-w-[850px] flex hover:font-extrabold overflow-hidden justify-center h-[380px] items-center gap-3 p-4 py-10"
+      className="hero w-full max-w-[850px] flex hover:font-extrabold justify-center h-[380px] items-center gap-3 p-4 py-10 relative"
       ref={ref}
       onMouseEnter={() => setIsHovered(true)}
-      onClick={() => setIsHovered(!isHovered)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="absolute top-0 left-0 w-full h-full "></div>
-      <Image
-        src={"/logo.svg"}
-        alt="AARKYV STUDIOS logo"
-        className="mix-blend-difference"
-        width={10}
-        height={10}
-      />
+      <div className="absolute font-light top-0 left-0 w-full h-full ">
+        <div className="absolute cross top-0 left-0 w-4 h-4 flex justify-center items-center">
+          +
+        </div>
+        <div className="absolute cross top-0 right-0 w-4 h-4 flex justify-center items-center">
+          +
+        </div>
+        <div className="absolute cross bottom-0 left-0 w-4 h-4 flex justify-center items-center">
+          +
+        </div>
+        <div className="absolute cross bottom-0 right-0 w-4 h-4 flex justify-center items-center">
+          +
+        </div>
+      </div>
+
       <video
         src={
           "https://pininfarina.it/wp-content/uploads/2021/04/Nuovo-video3.mp4"
@@ -35,9 +41,25 @@ const Hero: React.FC = () => {
         className="saturate-100 max-w-[850px] h-[100%] object-cover  "
         loop
         muted
-        style={{ position: "absolute", width: isHovered ? '100%' : '0%', zIndex: -1 }}
+        style={{
+          position: "absolute",
+          width: isHovered ? "100%" : "0%",
+          zIndex: -1,
+        }}
       />
-      <span className="mix-blend-difference">AARKYV STUDIOS</span>
+      <div className="flex flex-col items-center">
+        <span className="mix-blend-difference flex gap-3">
+          {" "}
+          <Image
+            src={"/logo.svg"}
+            alt="AARKYV STUDIOS logo"
+            width={10}
+            height={10}
+          />
+          AARKYV STUDIOS
+        </span>
+        <a href="#about" className="mt-3 text-[0.65rem] font-semibold z-50 bg-white text-black p-1.5 px-5 rounded-3xl">Learn More</a>
+      </div>
     </div>
   );
 };
