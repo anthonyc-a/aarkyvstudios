9f12cbd4bc68d47e2bce485c4c415186f8b1cac7
A <anthonyc.animba@gmail.com>
2023-11-09 18:09:09 +0700

    .

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -13,69 +13,69 @@ const Hero: React.FC = () => {
 
   return (
     <a href="#about">
- <div
-      className="hero w-full max-w-[850px] flex hover:font-extrabold justify-center h-[380px] items-center gap-3 p-4 py-10 relative"
-      ref={ref}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="absolute font-extralight text-[13px] top-0 left-0 w-full h-full ">
-        <div className="absolute cross top-0 left-0 w-4 h-4 flex justify-center items-center">
-          +
+      <div
+        className="hero w-full max-w-[850px] flex hover:font-extrabold justify-center h-[380px] items-center gap-3 p-4 py-10 relative"
+        ref={ref}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <div className="absolute font-extralight text-[13px] top-0 left-0 w-full h-full ">
+          <div className="absolute cross top-0 left-0 w-4 h-4 flex justify-center items-center">
+            +
+          </div>
+          <div className="absolute cross top-0 right-0 w-4 h-4 flex justify-center items-center">
+            +
+          </div>
+          <div className="absolute cross bottom-0 left-0 w-4 h-4 flex justify-center items-center">
+            +
+          </div>
+          <div className="absolute cross bottom-0 right-0 w-4 h-4 flex justify-center items-center">
+            +
+          </div>
         </div>
-        <div className="absolute cross top-0 right-0 w-4 h-4 flex justify-center items-center">
-          +
-        </div>
-        <div className="absolute cross bottom-0 left-0 w-4 h-4 flex justify-center items-center">
-          +
-        </div>
-        <div className="absolute cross bottom-0 right-0 w-4 h-4 flex justify-center items-center">
-          +
+
+        <video
+          src={
+            "https://pininfarina.it/wp-content/uploads/2021/04/Nuovo-video3.mp4"
+          }
+          autoPlay
+          className="max-w-[850px] backdrop-blur-sm h-[100%] object-cover  "
+          loop
+          muted
+          style={{
+            position: "absolute",
+            width: isHovered ? "100%" : "0%",
+            zIndex: -1,
+          }}
+        />
+        <div className="inner flex flex-col w-[240px] gap-[14px]">
+          <div className="logo-container mix-blend-difference flex gap-[16px]">
+            <Image
+              src={"/logo.svg"}
+              alt="AARKYV STUDIOS logo"
+              width={10}
+              height={10}
+            />
+            <span className="font-black">AARKYV STUDIOS</span>
+          </div>
+          <a
+            href="#about"
+            className="cta flex items-center justify-between rounded-[2px] text-[0.65rem] font-bold bg-white text-black p-[6px] px-4"
+          >
+            Learn More
+            <Image
+              src={"/arrow.svg"}
+              alt="arrow"
+              width={8}
+              height={8}
+              className={`${
+                isHovered && "translate-x-[2px] translate-y-[-1px]"
+              } transition-all duration-300`}
+            />
+          </a>
         </div>
       </div>
-
-      <video
-        src={
-          "https://pininfarina.it/wp-content/uploads/2021/04/Nuovo-video3.mp4"
-        }
-        autoPlay
-        className="max-w-[850px] backdrop-blur-sm h-[100%] object-cover  "
-        loop
-        muted
-        style={{
-          position: "absolute",
-          width: isHovered ? "100%" : "0%",
-          zIndex: -1,
-        }}
-      />
-      <div className="inner flex flex-col w-[240px] gap-[14px]">
-        <div className="logo-container mix-blend-difference flex gap-[16px]">
-          <Image
-            src={"/logo.svg"}
-            alt="AARKYV STUDIOS logo"
-            width={10}
-            height={10}
-          />
-          <span className="font-black">AARKYV STUDIOS</span>
-        </div>
-        <a
-          href="#about"
-          className="cta flex items-center justify-between rounded-[2px] text-[0.65rem] font-bold bg-white text-black p-[6px] px-4"
-        >
-          Learn More
-          <Image
-            src={"/arrow.svg"}
-            alt="arrow"
-            width={8}
-            height={8}
-            className={`${isHovered && "translate-x-[2px] translate-y-[-1px]"} transition-all duration-300`}
-          />
-        </a>
-      </div>
-    </div>
-
     </a>
-   
   );
 };
 
