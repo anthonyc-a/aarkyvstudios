c6aa262554bd6ed4c07a43a7bc1111bc6c14513a
A <anthonyc.animba@gmail.com>
2023-11-15 17:04:10 +0700

    .

--- app/components/marquee/index.tsx
+++ app/components/marquee/index.tsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 
 const MarqueeCpt = () => {
   return (
-    <div className="marquee z-50  clip max-w-[580px] overflow-hidden border border-[#000] text-[#000] p-1.5">
+    <div className="marquee mt-2 z-50  clip max-w-[580px] overflow-hidden border border-[#000] text-[#000] p-1.5">
       <Marquee speed={64} direction="left" gradient gradientColor="#fff" gradientWidth={24}>
         <div className="flex font-[600] text-[0.71rem] tracking-[0.05em]">
           <div>
