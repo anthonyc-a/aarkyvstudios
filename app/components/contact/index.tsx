7471d199cd4f5a067b1c1f435d4217daf6cfcf11
A <anthonyc.animba@gmail.com>
2023-11-15 19:39:39 +0700

    .

--- app/components/contact/index.tsx
+++ app/components/contact/index.tsx
@@ -45,7 +45,7 @@ const ContactButton = () => {
   return (
     <>
       <button
-        className={` p-1 px-10 bg-[#888] clip-sm z-10 ${
+        className={` p-1 px-10 bg-[#777] clip-sm z-10 ${
           showLogo ? "" : ""
         }`}
         onClick={handleOpenContactModal}
