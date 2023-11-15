e27f74e8dfc4baa3643d60d5660b066d55e50353
A <anthonyc.animba@gmail.com>
2023-11-15 16:47:04 +0700

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
             <div className="flex items-center gap-6">
               <Systems />
               <ThemeToggle />
-              <div className="menu border border-black font-light w-8 h-8 flex justify-center items-center text-3xl leading-[1] rounded-[4px]">
+              <div className="menu border border-black font-light w-8 h-8 flex justify-center items-center text-2xl leading-[1] rounded-[6px]">
                 =
               </div>
             </div>
