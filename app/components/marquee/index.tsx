6100f34a31a67b72a9f962e5d8e07c75e51f54c2
A <anthonyc.animba@gmail.com>
2023-11-15 16:42:15 +0700

    .

--- app/components/marquee/index.tsx
+++ app/components/marquee/index.tsx
@@ -5,7 +5,7 @@ const MarqueeCpt = () => {
   return (
     <div className="marquee z-50 w-[90%] clip max-w-[580px] overflow-hidden border border-[#000] text-[#000] mx-auto p-1.5">
       <Marquee speed={64} direction="left">
-        <div className="flex font-[550] text-[0.7rem] tracking-[0.05em]">
+        <div className="flex font-[550] text-[0.71rem] tracking-[0.05em]">
           <div>
             outstanding user experiences crafted with cutting-edge design and
             technology
