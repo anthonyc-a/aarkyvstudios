4ef58adfeab98ad0502440b26ae0e852995969c0
A <anthonyc.animba@gmail.com>
2023-11-19 14:00:52 +0700

    .

--- app/components/theme/index.tsx
+++ app/components/theme/index.tsx
@@ -28,7 +28,7 @@ const ThemeToggle = () => {
 
   return (
     <div className="flex gap-2 bg-white p-2 border border-[#222] rounded-[6px] hover:border-[#999] w-fit z-50">
-      <Image className="" src={"/moon.svg"} alt="" width={11} height={11} onClick={handleToggle} />
+      <Image className="" src={"/moon.svg"} alt="" width={10} height={10} onClick={handleToggle} />
     </div>
   );
 };
