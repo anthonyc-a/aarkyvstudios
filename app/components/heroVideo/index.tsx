5cf8ed9a97cce7f02b78f96a429dc81489c643d8
A <anthonyc.animba@gmail.com>
2023-11-20 02:07:14 +0700

    .

--- app/components/heroVideo/index.tsx
+++ app/components/heroVideo/index.tsx
@@ -27,7 +27,7 @@ const HeroVideo = () => {
 
   return (
     <div
-      className="flex relative flex-col justify-center h-[65%] mb-2 items-center md:w-1/2"
+      className="flex relative flex-col justify-center h-[68%] mb-2 items-center md:w-1/2"
       ref={containerRef}
     >
       <div className="relative video-container clip-lg h-full ">
